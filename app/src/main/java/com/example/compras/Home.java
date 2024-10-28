f047ccfcd70a4e7e7810db7b31a58fc77b05d65c
Manuela <141162551+ManuelaValencia28@users.noreply.github.com>
2024-10-28 16:28:51 -0500

    Botones usuario y admin

--- app/src/main/java/com/example/compras/Home.java
+++ app/src/main/java/com/example/compras/Home.java
@@ -2,13 +2,38 @@ package com.example.compras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+    private Button botonusuario, botonadmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        botonusuario = (Button)findViewById(R.id.botonusuario);
+        botonadmin = (Button) findViewById(R.id.botonadmin);
+
+        botonusuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, Login.class);
+                startActivity(intent);
+
+            }
+        });
+
+        botonadmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, LoginAdmin.class);
+                startActivity(intent);
+            }
+        });
     }
 }

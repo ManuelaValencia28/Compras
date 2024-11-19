437f61832d611e211e940c9a7d46258c8a46edb3
Manuela <141162551+ManuelaValencia28@users.noreply.github.com>
2024-11-19 13:41:23 -0500

    avance

--- app/src/main/java/com/example/compras/AgregarproductoActivity.java
+++ app/src/main/java/com/example/compras/AgregarproductoActivity.java
@@ -1,14 +1,88 @@
 package com.example.compras;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class AgregarproductoActivity extends AppCompatActivity {
+
+    private ImageView imagenpro;
+    private EditText nombrepro, descripcionpro, preciopro;
+    private Button agregarpro;
+    private static final int Gallery_Pick = 1;
+    private Uri imagenUri;
+    private String productoRandonKey, downloadUri;
+    private StorageReference ProductoImagenRef;
+    private DatabaseReference ProductoRef;
+    private ProgressDialog dialog;
+    private String Categoria, NombrePro, Desc, Precio ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregarproducto);
+
+        Categoria = getIntent().getExtras().get("categoria").toString();
+        ProductoImagenRef = FirebaseStorage.getInstance().getReference().child("Imagenes Productos");
+        ProductoRef = FirebaseDatabase.getInstance().getReference().child("Productos");
+
+        Toast.makeText(this, Categoria,Toast.LENGTH_SHORT).show();
+
+
+        imagenpro = (ImageView) findViewById(R.id.imagenpro);
+        nombrepro = (EditText) findViewById(R.id.nombrepro);
+        descripcionpro = (EditText) findViewById(R.id.descripcionpro);
+        preciopro = (EditText) findViewById(R.id.preciopro);
+        agregarpro = (Button) findViewById(R.id.agregarpro);
+        
+        imagenpro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AbrirGaleria();
+            }
+        });
+        agregarpro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ValidarProducto();
+            }
+        });
+
+    }
+
+    private void AbrirGaleria() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/");
+        startActivityForResult(intent, Gallery_Pick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == Gallery_Pick && resultCode == RESULT_OK && data != null){
+            imagenUri = data.getData();
+            imagenpro.setImageURI(imagenUri);
+
+
+        }
+    }
+
+    private void ValidarProducto() {
     }
 }
--- app/src/main/java/com/example/compras/FragmentUno.java
+++ app/src/main/java/com/example/compras/FragmentUno.java
@@ -1,5 +1,6 @@
 package com.example.compras;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,146 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentUno#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentUno extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private View fragmento;
+    private ImageView camisetas, apple, auriculares, bolsos;
+    private ImageView camaras, celulares, gafas, instrumentos;
+    private ImageView laptops, relojes, videojuegos, libros;
     public FragmentUno() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentUno.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentUno newInstance(String param1, String param2) {
-        FragmentUno fragment = new FragmentUno();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_uno, container, false);
+        fragmento =  inflater.inflate(R.layout.fragment_uno, container, false);
+
+       camisetas = (ImageView)fragmento.findViewById(R.id.camisetas);
+       apple = (ImageView)fragmento.findViewById(R.id.apple);
+       auriculares = (ImageView)fragmento.findViewById(R.id.auriculares);
+       bolsos = (ImageView)fragmento.findViewById(R.id.bolsos);
+       camaras = (ImageView)fragmento.findViewById(R.id.camaras);
+       celulares = (ImageView)fragmento.findViewById(R.id.celulares);
+       gafas = (ImageView)fragmento.findViewById(R.id.gafas);
+       instrumentos = (ImageView)fragmento.findViewById(R.id.instrumentos);
+       laptops = (ImageView)fragmento.findViewById(R.id.laptops);
+       relojes = (ImageView)fragmento.findViewById(R.id.relojes);
+       videojuegos = (ImageView)fragmento.findViewById(R.id.videojuegos);
+       libros = (ImageView)fragmento.findViewById(R.id.libros);
+
+
+       camisetas.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(getContext(), AgregarproductoActivity.class);
+               intent.putExtra("categoria", "camisetas");
+               startActivity(intent);
+           }
+       });
+
+        apple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AgregarproductoActivity.class);
+                intent.putExtra("categoria", "apple");
+                startActivity(intent);
+            }
+        });
+
+        auriculares.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AgregarproductoActivity.class);
+                intent.putExtra("categoria", "auriculares");
+                startActivity(intent);
+            }
+        });
+
+        bolsos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AgregarproductoActivity.class);
+                intent.putExtra("categoria", "bolsos");
+                startActivity(intent);
+            }
+        });
+
+        camaras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AgregarproductoActivity.class);
+                intent.putExtra("categoria", "camaras");
+                startActivity(intent);
+            }
+        });
+
+        celulares.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AgregarproductoActivity.class);
+                intent.putExtra("categoria", "celulares");
+                startActivity(intent);
+            }
+        });
+
+        gafas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AgregarproductoActivity.class);
+                intent.putExtra("categoria", "gafas");
+                startActivity(intent);
+            }
+        });
+
+        instrumentos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AgregarproductoActivity.class);
+                intent.putExtra("categoria", "instrumentos");
+                startActivity(intent);
+            }
+        });
+
+        laptops.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AgregarproductoActivity.class);
+                intent.putExtra("categoria", "laptops");
+                startActivity(intent);
+            }
+        });
+
+        relojes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AgregarproductoActivity.class);
+                intent.putExtra("categoria", "relojes");
+                startActivity(intent);
+            }
+        });
+
+        videojuegos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AgregarproductoActivity.class);
+                intent.putExtra("categoria", "videojuegos");
+                startActivity(intent);
+            }
+        });
+
+        libros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AgregarproductoActivity.class);
+                intent.putExtra("categoria", "libros");
+                startActivity(intent);
+            }
+        });
+
+        return fragmento;
     }
 }
